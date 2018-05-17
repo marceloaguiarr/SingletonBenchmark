a076a54ade8744161fd936c5d800c11611630255
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-05-17 11:17:05 -0300

    Created benchmarks

---
+++ src/main/java/org/marceloaguiarr/SingletonBenchmark.java
@@ -0,0 +1,34 @@
+package org.marceloaguiarr;
+
+import org.marceloaguiarr.util.EagerHolder;
+import org.marceloaguiarr.util.LazyHolderIdiom;
+import org.marceloaguiarr.util.Synchronized;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
+public class SingletonBenchmark {
+
+    @Benchmark
+    @Threads(1000)
+    public void SynchronizedBench() {
+        Synchronized.getInstance();
+    }
+
+    @Benchmark
+    @Threads(1000)
+    public void LazyHolderBench() {
+        LazyHolderIdiom.getInstance();
+    }
+
+    @Benchmark
+    @Threads(1000)
+    public void EagerHolderBench() {
+        EagerHolder.getInstance();
+    }
+}
