8843cf62478465fc43ca72eea4f8bc4273788e1c
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-05-17 11:17:17 -0300

    Created benchmark runner

---
+++ src/main/java/org/marceloaguiarr/SingletonBenchmarkRunner.java
@@ -0,0 +1,7 @@
+package org.marceloaguiarr;
+
+public class SingletonBenchmarkRunner {
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
